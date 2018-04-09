b714748d53e9840ec06a43cdd72ae88a0150548e
Mariusz Galek <mgalek1@gmail.com>
2018-04-09 20:31:44 +0200

    Add database connection

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/nodeappdatabase', {
+mongoose.connect('mongodb://MariuszGalek:54321@ds227858.mlab.com:27858/database-1', {
     useMongoClient: true
 });
 
